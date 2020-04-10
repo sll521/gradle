e9413044b9993319de92908b53bf5f326ca55a2d
Adam Murdoch <adam@gradle.com>
2020-04-10 14:22:32 +1000

    Fixes.

--- subprojects/model-core/src/integTest/groovy/org/gradle/api/provider/PropertyIntegrationTest.groovy
+++ subprojects/model-core/src/integTest/groovy/org/gradle/api/provider/PropertyIntegrationTest.groovy
@@ -236,7 +236,7 @@ task thing(type: SomeTask) {
 
         then:
         failure.assertHasDescription("Execution failed for task ':thing'.")
-        failure.assertHasCause("Could not calculate the value of task ':thing' property 'prop'.")
+        failure.assertHasCause("Failed to calculate the value of task ':thing' property 'prop'.")
         failure.assertHasCause("broken")
     }
 
